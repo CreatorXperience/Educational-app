e6a35141cef0c7b3b7d2319f1557372319769df9
CreatorXperience <allyearmustobey@gmail.com>
2023-09-09 17:52:12 +0100

    ft- removed create file

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,9 @@ import { useEffect } from "react";
 import "./App.css";
 import { database } from "./database/db";
 import { ref, set } from "firebase/database";
-import createCourse from "./database/crud/create";
 
 function App() {
-  useEffect(() => {
-    createCourse({
-      image: "image_url_15.jpg",
-      category: "design",
-      title: "Interaction Design: Beyond Human-Computer Interaction",
-      author: {
-        name: "Jenny Preece",
-        status: "interaction designer",
-        stars: 4.5,
-      },
-    });
-  });
+  useEffect(() => {});
 
   return <div className="App">App</div>;
 }

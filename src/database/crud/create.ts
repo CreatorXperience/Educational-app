60dc172b1ceeebe715e928df76fd8179c2df88e3
CreatorXperience <allyearmustobey@gmail.com>
2023-09-09 14:11:11 +0100

    ft: replace existing JSON

--- src/database/crud/create.ts
+++ src/database/crud/create.ts
@@ -14,14 +14,17 @@ type TCourse = {
 };
 
 const createCourse = (course: TCourse) => {
-  let newBookRef = ref(database, "books");
-  let pushRef = push(newBookRef);
-  console.log(pushRef);
+  try {
+    let newBookRef = ref(database, "books");
+    let pushRef = push(newBookRef);
 
-  set(pushRef, {
-    id: pushRef.key,
-    ...course,
-  });
+    set(pushRef, {
+      id: pushRef.key,
+      ...course,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default createCourse;

4921c45a7841a8fde44b38c7d87b0fcdcfdb88e6
CreatorXperience <allyearmustobey@gmail.com>
2023-09-10 22:03:46 +0100

    ft- cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -8,37 +8,7 @@ import { User } from "firebase/auth";
 import signInWithGitOauth from "./auth/gitOauth";
 
 function App() {
-  const [database, setDatabase] = useState<TDatabase[] | null>();
-  const [userInfo, setUserInfo] = useState<User | null>();
-  const { getUserInfo, signUp, signIn } = auth();
-
-  useEffect(() => {
-    signIn("allyearmustobey@gmail.com", "1234567890");
-
-    getUserInfo(setUserInfo);
-    readData(setDatabase);
-  }, []);
-
-  console.log(userInfo);
-
-  return (
-    <div className="App">
-      {database?.map((database) => {
-        return (
-          <div key={database.id}>
-            {" "}
-            <img
-              src={database.coverImage}
-              alt="image"
-              width="200px"
-              style={{ borderRadius: "20px" }}
-            />{" "}
-            <h4>{database.coverTitle}</h4>
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;

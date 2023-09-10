184fa721a98fd59e2d28cb65237a7ad2ae721610
CreatorXperience <allyearmustobey@gmail.com>
2023-09-10 22:35:43 +0100

    ch- starting home screen

---
+++ src/constants/constant.ts
@@ -0,0 +1,16 @@
+let FONT = {
+  xsm: "14px",
+  sm: "16px",
+  md: "22px",
+  lg: "70px",
+  xlg: "100px",
+};
+
+const COLORS = {
+  primary: "#2C1F4A",
+  primaryLight: "#3D2A5F",
+  secondary: "#7F56D9",
+  tertiary: "#FF5156",
+};
+
+export { FONT, COLORS };
